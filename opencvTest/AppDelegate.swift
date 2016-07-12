c97ef7dbd2cc62b6e495e574720366510f129834
Shohei Aoki <shoaok@gmail.com>
2016-07-12 11:08:30 +0900

    Initial Commit

---
+++ opencvTest/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  opencvTest
+//
+//  Created by shohei on 7/12/16.
+//  Copyright © 2016 shohei. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ opencvTest/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  opencvTest
+//
+//  Created by shohei on 7/12/16.
+//  Copyright © 2016 shohei. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
