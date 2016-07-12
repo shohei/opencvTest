89eacf04f5488024e2cfa6d84bcb5fff32293375
Shohei Aoki <shoaok@gmail.com>
2016-07-12 11:42:10 +0900

    initial commit

---
+++ opencvTest/Detector.h
@@ -0,0 +1,13 @@
+//
+//  Detector.h
+//  opencvTest
+//
+//  Created by shohei on 7/12/16.
+//  Copyright © 2016 shohei. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface Detector : NSViewController
+
+@end
